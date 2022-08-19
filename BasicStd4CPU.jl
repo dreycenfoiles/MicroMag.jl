bbbcd6be8b2e05dfffbca79a9bba2358aee92f80
Dreycen Foiles <foilesdreycen@gmail.com>
2022-08-19 11:57:01 -0500

    Going to change demag kernel calcultion

--- BasicStd4CPU.jl
+++ BasicStd4CPU.jl
@@ -1,27 +1,30 @@
 using DifferentialEquations
 using LinearAlgebra # Used for cross product
-using Plots 
+using Plots
 using Statistics # Used for mean 
 using DSP # Used for convolution
 using FFTW
+using Unitful
+
 using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 global_logger(TerminalLogger())
 
-# TODO: Scale problem?
-nx = 6; # number of cells on x direction
-ny = 6; # number of cells on y direction
+nx = 166; # number of cells on x direction
+ny = 42; # number of cells on y direction
 nz = 1;
 
-dx = 3; # cell size on x direction (nm)
-dy = 3; # cell size on y direction (nm)
-dz = 3; # cell size on z direction (nm)
 
-alpha = 0.5 
-A = 1.3e-11 * 1e18
-mu0 = pi * 4e-7 / 10
-Ms = 800
-gamma = .221
+
+dx = 3e-9u"nm"; # cell size on x direction (nm)
+dy = 3e-9u"nm"; # cell size on y direction (nm)
+dz = 3e-9u"nm"; # cell size on z direction (nm)
+
+alpha = 0.5
+A = 1.3e-11u"J/m"
+mu0 = pi * 4e-7u"H/m"
+Ms = 8e5u"A/m"
+gamma = 2.21e5u"m/(A*s)"
 
 
 Kxx = zeros(2 * nx, 2 * ny, 2 * nz) # Initialization of demagnetization tensors
@@ -73,13 +76,13 @@ fft_Kzz = fft(Kzz);
 # calculation of demag tensor done
 
 # Demag tensors are larger so they can have zero padding
-Hx_demag = zeros(2*nx, 2*ny, 2*nz)
-Hy_demag = zeros(2*nx, 2*ny, 2*nz)
-Hz_demag = zeros(2*nx, 2*ny, 2*nz)
+Hx_demag = zeros(2 * nx, 2 * ny, 2 * nz)
+Hy_demag = zeros(2 * nx, 2 * ny, 2 * nz)
+Hz_demag = zeros(2 * nx, 2 * ny, 2 * nz)
 
-mx_demag_buffer = zeros(2*nx, 2*ny, 2*nz)
-my_demag_buffer = zeros(2*nx, 2*ny, 2*nz)
-mz_demag_buffer = zeros(2*nx, 2*ny, 2*nz)
+mx_demag_buffer = zeros(2 * nx, 2 * ny, 2 * nz)
+my_demag_buffer = zeros(2 * nx, 2 * ny, 2 * nz)
+mz_demag_buffer = zeros(2 * nx, 2 * ny, 2 * nz)
 
 # TODO: Plan FFT 
 # TODO: Use optional Julia FFT implementation for autodifferentiation
@@ -100,11 +103,13 @@ end
 
 function LLG_loop(dm, m, p, t)
 
+    A, mu0, Ms, alpha, gamma, dx, dy, dz = p
 
-    A, mu0, Ms, alpha, gamma, precess, dx, dy, dz = p
+    H_ext = [1u"T", 1u"T", 1u"T"] / mu0 
+    precess=1
 
-    prefactor1 = -gamma / (1 + alpha * alpha)
-    prefactor2 = alpha / Ms
+    prefactor1 = -gamma * (1 + alpha^2)
+    prefactor2 = alpha/Ms
 
     mx = m[1, :, :, :]
     my = m[2, :, :, :]
@@ -125,29 +130,38 @@ function LLG_loop(dm, m, p, t)
         i, j, k = Tuple(index)
 
         # TODO: Periodic boundary conditions
-        # TODO: Implement boundary conditions for truly 3D system
-        # Neumann boundary conditions
-        if i == 1 || j == 1 || i == nx || j == ny
-            dm[1, i, j, k] = 0
-            dm[2, i, j, k] = 0
-            dm[3, i, j, k] = 0
-            continue
-        end
+
 
         #TODO: Add 3rd dimsion to exchange field calulation
         #TODO: Use ParallelStencil.jl to speed up the calculation
-        mx_diff = (m[1, i+1, j] - 2 * m[1, i, j] + m[1, i-1, j]) / dx / dx + (m[1, i, j+1] - 2 * m[1, i, j] + m[1, i, j-1]) / dy / dy
-        my_diff = (m[2, i+1, j] - 2 * m[2, i, j] + m[2, i-1, j]) / dx / dx + (m[2, i, j+1] - 2 * m[2, i, j] + m[1, i, j-1]) / dy / dy
-        mz_diff = (m[3, i+1, j] - 2 * m[3, i, j] + m[3, i-1, j]) / dx / dx + (m[3, i, j+1] - 2 * m[3, i, j] + m[1, i, j-1]) / dy / dy
+        #TODO: Implement boundary conditions for truly 3D system
+
+        # Neumann boundary conditions
+        if (i == 1 || i == nx) && !(j == 1 || j == ny)
+
+            mx_diff = (m[1, i, j+1] - 2 * m[1, i, j] + m[1, i, j-1]) / dy / dy
+            my_diff = (m[2, i, j+1] - 2 * m[2, i, j] + m[1, i, j-1]) / dy / dy
+            mz_diff = (m[3, i, j+1] - 2 * m[3, i, j] + m[1, i, j-1]) / dy / dy
+
+        elseif (j == 1 || j == ny) && !(i == 1 || i == nx)
+            mx_diff = (m[1, i+1, j] - 2 * m[1, i, j] + m[1, i-1, j]) / dx / dx
+            my_diff = (m[2, i+1, j] - 2 * m[2, i, j] + m[2, i-1, j]) / dx / dx
+            mz_diff = (m[3, i+1, j] - 2 * m[3, i, j] + m[3, i-1, j]) / dx / dx
+
+        elseif (i == 1 || i == nx) && (j == 1 || j == ny)
+            mx_diff = 0
+            my_diff = 0
+            mz_diff = 0
+
+        else
+            mx_diff = (m[1, i+1, j] - 2 * m[1, i, j] + m[1, i-1, j]) / dx / dx + (m[1, i, j+1] - 2 * m[1, i, j] + m[1, i, j-1]) / dy / dy
+            my_diff = (m[2, i+1, j] - 2 * m[2, i, j] + m[2, i-1, j]) / dx / dx + (m[2, i, j+1] - 2 * m[2, i, j] + m[1, i, j-1]) / dy / dy
+            mz_diff = (m[3, i+1, j] - 2 * m[3, i, j] + m[3, i-1, j]) / dx / dx + (m[3, i, j+1] - 2 * m[3, i, j] + m[1, i, j-1]) / dy / dy
+
+        end
 
         #TODO: Use harmonic mean for spatially dependent exchange field
-        H_exch = 2 * A / (mu0 * Ms * Ms) .* [mx_diff, my_diff, mz_diff]
-
-        #TODO: Make this a passed variable
-        # H_ext = [-24.6e-3, 4.3e-3, 0] * 1e-9 * 1e-9 / mu0 # External field in 
-        # H_ext = [1, 1, 1]
-        H_ext = [100, 100, 100] 
-
+        H_exch = 2 * A / (Ms * mu0) .* [mx_diff, my_diff, mz_diff]
 
         #TODO: Make more concise vector expression
         H_effx = Hx_demag[i, j, k] + H_exch[1] + H_ext[1] # effective field
@@ -163,45 +177,84 @@ function LLG_loop(dm, m, p, t)
 
         # TODO: Spatially dependent damping
         dm[:, i, j, k] = prefactor1 .* (precess * cross(current_m, H_eff) +
-                                        prefactor2 .* cross(current_m, cross(current_m, H_eff))) / 200000000
+                                        prefactor2 .* cross(current_m, cross(current_m, H_eff))) / 1e9
 
-
+        # println(dm[2, i, j, k])
 
     end # end of LLG loop
 end
 
+function normalize!(m,Ms)
+    
+    mx = m[1, :, :, :]
+    my = m[2, :, :, :]
+    mz = m[3, :, :, :]
+
+    mag_length = sqrt.(mx.^2 + my.^2 + mz.^2)
+    mx ./= mag_length .* Ms 
+    my ./= mag_length .* Ms
+    mz ./= mag_length .* Ms
+
+end 
+
 
 m0 = zeros(3, nx, ny, nz)
 m0[1, :, :, :] .= Ms
-# m0[1, :, :, :] .= .1 * Ms # initial magnetization
-# m0[2, :, :, :] .= 0.1 * Ms # initial magnetization
 
-# dm_dummy = zeros(3, nx, ny, nz)
-p = (A, mu0, Ms, alpha, gamma, 1, dx, dy, dz)
+p = (A, mu0, Ms, alpha, gamma, dx, dy, dz)
 
-end_point = .75
+function condition(m, t, integrator)
+
+    mx = m[1, :, :, :]
+    my = m[2, :, :, :]
+    mz = m[3, :, :, :]
+
+    A, mu0, Ms, alpha, gamma, dx, dy, dz = integrator.p
+    return mean(sqrt.(mx .^ 2 + my .^ 2 + mz .^ 2)) != Ms
+end
+
+function affect!(integrator)
+
+    A, mu0, Ms, alpha, gamma, dx, dy, dz = integrator.p
+
+    mx = integrator.u[1, :, :, :]
+    my = integrator.u[2, :, :, :]
+    mz = integrator.u[3, :, :, :]
+
+    mag_length = sqrt.(mx .^ 2 + my .^ 2 + mz .^ 2)
+    # @info("Magnetization length: %f", mx ./ mag_length .* Ms)
+    integrator.u[1, :, :, :] ./= mag_length .* Ms
+    integrator.u[2, :, :, :] ./= mag_length .* Ms
+    integrator.u[3, :, :, :] ./= mag_length .* Ms
+
+    @info("Magnetization length: %f", integrator.u[1, :, :, :])
+end
+
+cb = DiscreteCallback(condition,affect!)
+
+end_point = 30e-9
 tspan = (0, end_point)
+t_range = range(0, end_point, length=300)
+
+# norm_manifold = ManifoldProjection(norm_residual)
 
 # prob = SteadyStateProblem(LLG_loop, m0, p)
-# @time sol = solve(prob, DynamicSS(BS3()))
-
-# p = (A, mu0, Ms, alpha, gamma, 1, dx, dy, dz)
-
 prob = ODEProblem(LLG_loop, m0, tspan, p)
-@time sol = solve(prob, BS3(), progress=true,progress_steps=100)
 
-t_range = range(0, end_point, length=1000)
+@time sol = solve(prob, Tsit5(), progress=true, progress_steps=100,dt=1e-12)
+
 
 mx_vals = sol(t_range)[1, :, :, :, :]
-my_vals = sol(t_range)[2, :, :, :, :] 
-mz_vals = sol(t_range)[3, :, :, :, :] 
+my_vals = sol(t_range)[2, :, :, :, :]
+mz_vals = sol(t_range)[3, :, :, :, :]
 
 mx_avg = mean(mx_vals, dims=[1, 2, 3])[1, 1, 1, :]
 my_avg = mean(my_vals, dims=[1, 2, 3])[1, 1, 1, :]
 mz_avg = mean(mz_vals, dims=[1, 2, 3])[1, 1, 1, :]
 
-norm = sqrt.(mx_avg.^ 2 + my_avg.^2 + mz_avg.^2)
+mag = sqrt.(mx_avg .^ 2 + my_avg .^ 2 + mz_avg .^ 2)
 
-plot(t_range, mx_avg./norm, label="mx")
-plot!(t_range, my_avg./norm, label="my")
-plot!(t_range, mz_avg./norm, label="mz")
+plot(t_range, mx_avg, label="mx")
+plot!(t_range, my_avg, label="my")
+plot!(t_range, mz_avg, label="mz")
+plot!(t_range, mag, label="norm")
