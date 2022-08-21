057b590fe6aebf77da78ec559632de79f1a25a49
Dreycen Foiles <foilesdreycen@gmail.com>
2022-08-20 19:42:15 -0500

    IT WORKS!!

--- BasicStd4CPU.jl
+++ BasicStd4CPU.jl
@@ -4,27 +4,27 @@ using Plots
 using Statistics # Used for mean 
 using DSP # Used for convolution
 using FFTW
-using Unitful
+using ModelingToolkit
 
 using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 global_logger(TerminalLogger())
 
-nx = 166; # number of cells on x direction
-ny = 42; # number of cells on y direction
+nx = 30; # number of cells on x direction
+ny = 20; # number of cells on y direction
 nz = 1;
 
 
 
-dx = 3e-9u"nm"; # cell size on x direction (nm)
-dy = 3e-9u"nm"; # cell size on y direction (nm)
-dz = 3e-9u"nm"; # cell size on z direction (nm)
+dx = 3; # cell size on x direction (nm)
+dy = 3 # cell size on y direction (nm)
+dz = 3; # cell size on z direction (nm)
 
 alpha = 0.5
-A = 1.3e-11u"J/m"
-mu0 = pi * 4e-7u"H/m"
-Ms = 8e5u"A/m"
-gamma = 2.21e5u"m/(A*s)"
+A = 1.3e-11 * 1e18
+mu0 = 1.256
+Ms = 800
+gamma = 0.221
 
 
 Kxx = zeros(2 * nx, 2 * ny, 2 * nz) # Initialization of demagnetization tensors
@@ -33,6 +33,148 @@ Kxz = zeros(2 * nx, 2 * ny, 2 * nz)
 Kyy = zeros(2 * nx, 2 * ny, 2 * nz)
 Kyz = zeros(2 * nx, 2 * ny, 2 * nz)
 Kzz = zeros(2 * nx, 2 * ny, 2 * nz)
+
+
+#Numerical Micromagnetics: Finite Difference Methods, Jacques E. Miltat1 and Michael J. Donahue. Page 14.
+
+function newell_f(x, y, z)
+    x2 = x * x
+    y2 = y * y
+    z2 = z * z
+    R = sqrt(x2 + y2 + z2)
+    if R == 0.0
+        return 0.0
+    end
+
+    f = 1.0 / 6 * (2 * x2 - y2 - z2) * R
+
+    if x2 > 0
+        f -= x * y * z * atan(y * z / (x * R))
+    end
+
+    if x2 + z2 > 0
+        f += 0.5 * y * (z2 - x2) * asinh(y / (sqrt(x2 + z2)))
+    end
+
+    if x2 + y2 > 0
+        f += 0.5 * z * (y2 - x2) * asinh(z / (sqrt(x2 + y2)))
+    end
+    return f
+end
+
+
+function newell_g(x, y, z)
+    x2 = x * x
+    y2 = y * y
+    z2 = z * z
+
+    R = sqrt(x2 + y2 + z2)
+    if R == 0.0
+        return 0.0
+    end
+
+    g = -1.0 / 3 * x * y * R
+
+    if z2 > 0
+        g -= 1.0 / 6 * z2 * z * atan(x * y / (z * R))
+    end
+    if y2 > 0
+        g -= 0.5 * y2 * z * atan(x * z / (y * R))
+    end
+    if x2 > 0
+        g -= 0.5 * x2 * z * atan(y * z / (x * R))
+    end
+
+    if x2 + y2 > 0
+        g += x * y * z * asinh(z / (sqrt(x2 + y2)))
+    end
+
+    if y2 + z2 > 0
+        g += 1.0 / 6 * y * (3 * z2 - y2) * asinh(x / (sqrt(y2 + z2)))
+    end
+
+    if x2 + z2 > 0
+        g += 1.0 / 6 * x * (3 * z2 - x2) * asinh(y / (sqrt(x2 + z2)))
+    end
+
+    return g
+end
+
+
+function demag_tensor_diag(x, y, z, dx, dy, dz)
+
+    tensor = 8.0 * newell_f(x, y, z)
+
+    tensor -= 4.0 * newell_f(x + dx, y, z)
+    tensor -= 4.0 * newell_f(x - dx, y, z)
+    tensor -= 4.0 * newell_f(x, y - dy, z)
+    tensor -= 4.0 * newell_f(x, y + dy, z)
+    tensor -= 4.0 * newell_f(x, y, z - dz)
+    tensor -= 4.0 * newell_f(x, y, z + dz)
+
+    tensor += 2.0 * newell_f(x + dx, y + dy, z)
+    tensor += 2.0 * newell_f(x + dx, y - dy, z)
+    tensor += 2.0 * newell_f(x - dx, y - dy, z)
+    tensor += 2.0 * newell_f(x - dx, y + dy, z)
+    tensor += 2.0 * newell_f(x + dx, y, z + dz)
+    tensor += 2.0 * newell_f(x + dx, y, z - dz)
+    tensor += 2.0 * newell_f(x - dx, y, z + dz)
+    tensor += 2.0 * newell_f(x - dx, y, z - dz)
+    tensor += 2.0 * newell_f(x, y - dy, z - dz)
+    tensor += 2.0 * newell_f(x, y - dy, z + dz)
+    tensor += 2.0 * newell_f(x, y + dy, z + dz)
+    tensor += 2.0 * newell_f(x, y + dy, z - dz)
+
+    tensor -= newell_f(x + dx, y + dy, z + dz)
+    tensor -= newell_f(x + dx, y + dy, z - dz)
+    tensor -= newell_f(x + dx, y - dy, z + dz)
+    tensor -= newell_f(x + dx, y - dy, z - dz)
+    tensor -= newell_f(x - dx, y + dy, z + dz)
+    tensor -= newell_f(x - dx, y + dy, z - dz)
+    tensor -= newell_f(x - dx, y - dy, z + dz)
+    tensor -= newell_f(x - dx, y - dy, z - dz)
+
+    return tensor / (4.0 * pi * dx * dy * dz)
+
+end
+
+function demag_tensor_off_diag(x, y, z, dx, dy, dz)
+
+    tensor = 8.0 * newell_g(x, y, z)
+
+    tensor -= 4.0 * newell_g(x + dx, y, z)
+    tensor -= 4.0 * newell_g(x - dx, y, z)
+    tensor -= 4.0 * newell_g(x, y - dy, z)
+    tensor -= 4.0 * newell_g(x, y + dy, z)
+    tensor -= 4.0 * newell_g(x, y, z - dz)
+    tensor -= 4.0 * newell_g(x, y, z + dz)
+
+
+    tensor += 2.0 * newell_g(x + dx, y + dy, z)
+    tensor += 2.0 * newell_g(x + dx, y - dy, z)
+    tensor += 2.0 * newell_g(x - dx, y - dy, z)
+    tensor += 2.0 * newell_g(x - dx, y + dy, z)
+    tensor += 2.0 * newell_g(x + dx, y, z + dz)
+    tensor += 2.0 * newell_g(x + dx, y, z - dz)
+    tensor += 2.0 * newell_g(x - dx, y, z + dz)
+    tensor += 2.0 * newell_g(x - dx, y, z - dz)
+    tensor += 2.0 * newell_g(x, y - dy, z - dz)
+    tensor += 2.0 * newell_g(x, y - dy, z + dz)
+    tensor += 2.0 * newell_g(x, y + dy, z + dz)
+    tensor += 2.0 * newell_g(x, y + dy, z - dz)
+
+    tensor -= newell_g(x + dx, y + dy, z + dz)
+    tensor -= newell_g(x + dx, y + dy, z - dz)
+    tensor -= newell_g(x + dx, y - dy, z + dz)
+    tensor -= newell_g(x + dx, y - dy, z - dz)
+    tensor -= newell_g(x - dx, y + dy, z + dz)
+    tensor -= newell_g(x - dx, y + dy, z - dz)
+    tensor -= newell_g(x - dx, y - dy, z + dz)
+    tensor -= newell_g(x - dx, y - dy, z - dz)
+
+    return tensor / (4.0 * pi * dx * dy * dz)
+
+end
 
 # Calculate demagnetization tensor
 for index in CartesianIndices((range(-nx + 1, step=1, nx - 1), range(-ny + 1, step=1, ny - 1), range(-nz + 1, step=1, nz - 1)))
@@ -43,27 +185,22 @@ for index in CartesianIndices((range(-nx + 1, step=1, nx - 1), range(-ny + 1, st
         continue
     end
 
+    x = I * dx
+    y = J * dy
+    z = K * dz
+
     L = I + nx # shift the indices, b/c no negative index allowed in Julia
     M = J + ny
     N = K + nz
 
-    for i = 0:1 # helper indices
-        for j = 0:1
-            for k = 0:1
+    Kxx[L, M, N] += demag_tensor_diag(x, y, z, dx, dy, dz)
+    Kyy[L, M, N] += demag_tensor_diag(y, z, x, dx, dy, dz)
+    Kzz[L, M, N] += demag_tensor_diag(z, x, y, dx, dy, dz)
 
-                r = sqrt((I + i - 0.5) * (I + i - 0.5) * dx * dx + (J + j - 0.5) * (J + j - 0.5) * dy * dy + (K + k - 0.5) * (K + k - 0.5) * dz * dz)
+    Kxy[L, M, N] += demag_tensor_off_diag(x, y, z, dx, dy, dz)
+    Kxz[L, M, N] += demag_tensor_off_diag(y, z, x, dx, dy, dz)
+    Kyz[L, M, N] += demag_tensor_off_diag(z, x, y, dx, dy, dz)
 
-                Kxx[L, M, N] += 1 / 4 / pi * (-1)^(i + j + k) * atan((K + k - 0.5) * (J + j - 0.5) * dy * dz / r / (I + i - 0.5))
-                Kyy[L, M, N] += 1 / 4 / pi * (-1)^(i + j + k) * atan((I + i - 0.5) * (K + k - 0.5) * dx * dz / r / (J + j - 0.5))
-                Kzz[L, M, N] += 1 / 4 / pi * (-1)^(i + j + k) * atan((J + j - 0.5) * (I + i - 0.5) * dx * dy / r / (K + k - 0.5))
-
-                Kxy[L, M, N] += -1 / 4 / pi * (-1)^(i + j + k) * log((K + k - 0.5) * dz + r)
-                Kxz[L, M, N] += -1 / 4 / pi * (-1)^(i + j + k) * log((J + j - 0.5) * dy + r)
-                Kyz[L, M, N] += -1 / 4 / pi * (-1)^(i + j + k) * log((I + i - 0.5) * dz + r)
-
-            end
-        end
-    end
 end
 
 fft_Kxx = fft(Kxx);
@@ -84,6 +221,18 @@ mx_demag_buffer = zeros(2 * nx, 2 * ny, 2 * nz)
 my_demag_buffer = zeros(2 * nx, 2 * ny, 2 * nz)
 mz_demag_buffer = zeros(2 * nx, 2 * ny, 2 * nz)
 
+function neumann_bc(x, n)
+
+    if x == n + 1
+        return x - 1
+    elseif x == 0
+        return x + 1
+    else
+        return x
+    end
+end
+
+
 # TODO: Plan FFT 
 # TODO: Use optional Julia FFT implementation for autodifferentiation
 function convolve_kernel(fft_kernel1, fft_kernel2, fft_kernel3, mag1, mag2, mag3)
@@ -101,19 +250,19 @@ function convolve_kernel(fft_kernel1, fft_kernel2, fft_kernel3, mag1, mag2, mag3
 end
 
 
-function LLG_loop(dm, m, p, t)
+function LLG_loop(dm, m0, p, t)
 
     A, mu0, Ms, alpha, gamma, dx, dy, dz = p
 
-    H_ext = [1u"T", 1u"T", 1u"T"] / mu0 
-    precess=1
+    H_ext = [100, 100, 100]
+    precess = 1
 
     prefactor1 = -gamma * (1 + alpha^2)
-    prefactor2 = alpha/Ms
+    prefactor2 = alpha / Ms
 
-    mx = m[1, :, :, :]
-    my = m[2, :, :, :]
-    mz = m[3, :, :, :]
+    mx = m0[1, :, :, :]
+    my = m0[2, :, :, :]
+    mz = m0[3, :, :, :]
 
     for index in CartesianIndices((nx, ny, nz))
         mx_demag_buffer[index] = mx[index]
@@ -137,36 +286,26 @@ function LLG_loop(dm, m, p, t)
         #TODO: Implement boundary conditions for truly 3D system
 
         # Neumann boundary conditions
-        if (i == 1 || i == nx) && !(j == 1 || j == ny)
 
-            mx_diff = (m[1, i, j+1] - 2 * m[1, i, j] + m[1, i, j-1]) / dy / dy
-            my_diff = (m[2, i, j+1] - 2 * m[2, i, j] + m[1, i, j-1]) / dy / dy
-            mz_diff = (m[3, i, j+1] - 2 * m[3, i, j] + m[1, i, j-1]) / dy / dy
+        ip1 = neumann_bc(i + 1, nx)
+        im1 = neumann_bc(i - 1, nx)
+        jp1 = neumann_bc(j + 1, ny)
+        jm1 = neumann_bc(j - 1, ny)
 
-        elseif (j == 1 || j == ny) && !(i == 1 || i == nx)
-            mx_diff = (m[1, i+1, j] - 2 * m[1, i, j] + m[1, i-1, j]) / dx / dx
-            my_diff = (m[2, i+1, j] - 2 * m[2, i, j] + m[2, i-1, j]) / dx / dx
-            mz_diff = (m[3, i+1, j] - 2 * m[3, i, j] + m[3, i-1, j]) / dx / dx
+        # Laplacian operator
+        m_diff = [
+            (m0[comp, ip1, j] - 2 * m0[comp, i, j] + m0[comp, im1, j]) / dx / dx +
+            (m0[comp, i, jp1] - 2 * m0[comp, i, j] + m0[comp, i, jm1]) / dy / dy for comp in 1:3
+        ]
 
-        elseif (i == 1 || i == nx) && (j == 1 || j == ny)
-            mx_diff = 0
-            my_diff = 0
-            mz_diff = 0
-
-        else
-            mx_diff = (m[1, i+1, j] - 2 * m[1, i, j] + m[1, i-1, j]) / dx / dx + (m[1, i, j+1] - 2 * m[1, i, j] + m[1, i, j-1]) / dy / dy
-            my_diff = (m[2, i+1, j] - 2 * m[2, i, j] + m[2, i-1, j]) / dx / dx + (m[2, i, j+1] - 2 * m[2, i, j] + m[1, i, j-1]) / dy / dy
-            mz_diff = (m[3, i+1, j] - 2 * m[3, i, j] + m[3, i-1, j]) / dx / dx + (m[3, i, j+1] - 2 * m[3, i, j] + m[1, i, j-1]) / dy / dy
-
-        end
 
         #TODO: Use harmonic mean for spatially dependent exchange field
-        H_exch = 2 * A / (Ms * mu0) .* [mx_diff, my_diff, mz_diff]
+        H_exch = 2 * A / (Ms^2 * mu0) .* m_diff
 
         #TODO: Make more concise vector expression
         H_effx = Hx_demag[i, j, k] + H_exch[1] + H_ext[1] # effective field
-        H_effy = Hy_demag[i, j, k] + H_exch[2] + H_ext[2] # effective field
-        H_effz = Hz_demag[i, j, k] + H_exch[3] + H_ext[3] # effective field
+        H_effy = Hy_demag[i, j, k] + H_exch[2] + H_ext[2]
+        H_effz = Hz_demag[i, j, k] + H_exch[3] + H_ext[3]
 
         H_eff = [H_effx, H_effy, H_effz]
 
@@ -177,71 +316,29 @@ function LLG_loop(dm, m, p, t)
 
         # TODO: Spatially dependent damping
         dm[:, i, j, k] = prefactor1 .* (precess * cross(current_m, H_eff) +
-                                        prefactor2 .* cross(current_m, cross(current_m, H_eff))) / 1e9
+                                        prefactor2 .* cross(current_m, cross(current_m, H_eff)))
 
-        # println(dm[2, i, j, k])
+        # dm[4, i, j, k] = sqrt.(mx.^2 + my.^2 + mz.^2) - Ms
 
     end # end of LLG loop
 end
 
-function normalize!(m,Ms)
-    
-    mx = m[1, :, :, :]
-    my = m[2, :, :, :]
-    mz = m[3, :, :, :]
 
-    mag_length = sqrt.(mx.^2 + my.^2 + mz.^2)
-    mx ./= mag_length .* Ms 
-    my ./= mag_length .* Ms
-    mz ./= mag_length .* Ms
+p = (A, mu0, Ms, alpha, gamma, dx, dy, dz)
 
-end 
-
+end_point = 0.5
+tspan = (0, end_point)
 
 m0 = zeros(3, nx, ny, nz)
 m0[1, :, :, :] .= Ms
 
-p = (A, mu0, Ms, alpha, gamma, dx, dy, dz)
-
-function condition(m, t, integrator)
-
-    mx = m[1, :, :, :]
-    my = m[2, :, :, :]
-    mz = m[3, :, :, :]
-
-    A, mu0, Ms, alpha, gamma, dx, dy, dz = integrator.p
-    return mean(sqrt.(mx .^ 2 + my .^ 2 + mz .^ 2)) != Ms
-end
-
-function affect!(integrator)
-
-    A, mu0, Ms, alpha, gamma, dx, dy, dz = integrator.p
-
-    mx = integrator.u[1, :, :, :]
-    my = integrator.u[2, :, :, :]
-    mz = integrator.u[3, :, :, :]
-
-    mag_length = sqrt.(mx .^ 2 + my .^ 2 + mz .^ 2)
-    # @info("Magnetization length: %f", mx ./ mag_length .* Ms)
-    integrator.u[1, :, :, :] ./= mag_length .* Ms
-    integrator.u[2, :, :, :] ./= mag_length .* Ms
-    integrator.u[3, :, :, :] ./= mag_length .* Ms
-
-    @info("Magnetization length: %f", integrator.u[1, :, :, :])
-end
-
-cb = DiscreteCallback(condition,affect!)
-
-end_point = 30e-9
-tspan = (0, end_point)
-t_range = range(0, end_point, length=300)
-
-# norm_manifold = ManifoldProjection(norm_residual)
-
-# prob = SteadyStateProblem(LLG_loop, m0, p)
 prob = ODEProblem(LLG_loop, m0, tspan, p)
 
-@time sol = solve(prob, Tsit5(), progress=true, progress_steps=100,dt=1e-12)
+dt = 1e-3
+t_range = range(0, end_point, length=100)
+
+
+@time sol = solve(prob, Tsit5(), progress=true, progress_steps=100)
 
 
 mx_vals = sol(t_range)[1, :, :, :, :]
@@ -252,9 +349,9 @@ mx_avg = mean(mx_vals, dims=[1, 2, 3])[1, 1, 1, :]
 my_avg = mean(my_vals, dims=[1, 2, 3])[1, 1, 1, :]
 mz_avg = mean(mz_vals, dims=[1, 2, 3])[1, 1, 1, :]
 
-mag = sqrt.(mx_avg .^ 2 + my_avg .^ 2 + mz_avg .^ 2)
+m_norm = sqrt.(mx_avg .^ 2 + my_avg .^ 2 + mz_avg .^ 2)
 
 plot(t_range, mx_avg, label="mx")
 plot!(t_range, my_avg, label="my")
 plot!(t_range, mz_avg, label="mz")
-plot!(t_range, mag, label="norm")
+plot!(t_range, m_norm, label="norm")
