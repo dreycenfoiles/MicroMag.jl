472fdb4a0d9773491e6c9710bd6215059b84cb13
Dreycen Foiles <foilesdreycen@gmail.com>
2022-09-05 12:16:56 -0500

    GPU Kernel

--- Exchange.jl
+++ Exchange.jl
@@ -1,61 +1,71 @@
+function neumann_bc(x, n)
 
-
-
-# function δx!(H_exch, M, dx)
-#     @views @. H_exch[:, 2:end-1, :, :] += M[:, 3:end, :, :] - 2 * M[:, 2:end-1, :, :] + M[:, 1:end-2, :, :]
-#     @views @. H_exch[:, 1, :, :] += M[:, 2, :, :] - 2 * M[:, 1, :, :] + M[:, 1, :, :]
-#     @views @. H_exch[:, end, :, :] += M[:, end, :, :] - 2 * M[:, end, :, :] + M[:, end-1, :, :]
-#     H_exch ./ dx^2
-#     nothing
-# end
-
-# function δy!(H_exch, M, dy)
-#     @views @. H_exch[:, :, 2:end-1, :] += M[:, :, 3:end, :] - 2 * M[:, :, 2:end-1, :] + M[:, :, 1:end-2, :]
-#     @views @. H_exch[:, :, 1, :] += M[:, :, 2, :] - 2 * M[:, :, 1, :] + M[:, :, 1, :]
-#     @views @. H_exch[:, :, end, :] += M[:, :, end, :] - 2 * M[:, :, end, :] + M[:, :, end-1, :]
-#     H_exch ./ dy^2
-#     nothing
-# end
-
-# function δz!(H_exch, M, dz)
-#     @views @. H_exch[:, :, :, 2:end-1] += M[:, :, :, 3:end] - 2 * M[:, :, :, 2:end-1] + M[:, :, :, 1:end-2]
-#     @views @. H_exch[:, :, :, 1] += M[:, :, :, 2] - 2 * M[:, :, :, 1] + M[:, :, :, 1]
-#     @views @. H_exch[:, :, :, end] += M[:, :, :, end] - 2 * M[:, :, :, end] + M[:, :, :, end-1]
-#     H_exch ./ dz^2
-#     nothing
-# end
-
-# function Exchange!(H_exch, M, exch, dx, dy, dz)
-
-#     δx!(H_exch, M, dx)
-#     δy!(H_exch, M, dy)
-#     # δz!(H_exch, M, dz)
-
-#     H_exch .*= exch
-#     nothing
-# end
-
-function Exchange!(H_exch, M, exch, H0, H1, H2, H3, dd)
-    # calculation of exchange field
-
-    fill!(H0, 0)
-    fill!(H1, 0)
-    fill!(H2, 0)
-    fill!(H3, 0)
-
-    H0[:, 2:end, :, :] .= @views M[:, 1:end-1, :, :]
-    H0[:, 1, :, :] .= @views H0[:, 2, :, :]
-    H1[:, 1:end-1, :, :] .= @views M[:, 2:end, :, :]
-    H1[:, end, :, :] .= @views H1[:, end-1, :, :]
-
-    H2[:, :, 2:end, :] .= @views M[:, :, 1:end-1, :]
-    H2[:, :, 1, :] .= @views H2[:, :, 2, :]
-    H3[:, :, 1:end-1, :] .= @views M[:, :, 2:end, :]
-    H3[:, :, end, :] .= @views H3[:, :, end-1, :]
-
-    H_exch .+= @views @. exch / dd / dd * (H0 + H1 + H2 + H3 - 4 * M)
+    if x == n + 1
+        return x - 1
+    elseif x == 0
+        return x + 1
+    else
+        return x
+    end
 
 end
+
+# TODO: Periodic boundary conditions
+
+
+function Exchange_kernel(H_exch, M, dx, dy, dz)
+
+    nc, nx, ny, nz = size(M)
+
+    c = blockIdx().x 
+    j = blockIdx().y
+    i = threadIdx().x
+
+    ip1 = neumann_bc(i + 1, nx)
+    im1 = neumann_bc(i - 1, nx)
+    jp1 = neumann_bc(j + 1, ny)
+    jm1 = neumann_bc(j - 1, ny)
+
+   H_exch[c, i, j, 1] = (M[c, ip1, j, 1] - 2 * M[c, i, j, 1] + M[c, im1, j, 1]) / dx / dx +
+                        (M[c, i, jp1, 1] - 2 * M[c, i, j, 1] + M[c, i, jm1, 1]) / dy / dy
+
+    nothing
+end 
+
+function Exchange!(H_exch, M, exch, dx, dy, dz)
+
+    nc, nx, ny, nz = size(M)
+
+    # FIXME: Will error if nx >= 1024
+    @cuda blocks=(nc, ny) threads=nx Exchange_kernel(H_exch, M, dx, dy, dz)
+
+    H_exch .*= exch
+
+    return
+end
+
+
+# function Exchange!(H_exch, M, exch, H0, H1, H2, H3, dd)
+#     # calculation of exchange field
+
+#     fill!(H0, 0)
+#     fill!(H1, 0)
+#     fill!(H2, 0)
+#     fill!(H3, 0)
+
+#     H0[:, 2:end, :, :] .= @views M[:, 1:end-1, :, :]
+#     H0[:, 1, :, :] .= @views H0[:, 2, :, :]
+#     H1[:, 1:end-1, :, :] .= @views M[:, 2:end, :, :]
+#     H1[:, end, :, :] .= @views H1[:, end-1, :, :]
+
+#     H2[:, :, 2:end, :] .= @views M[:, :, 1:end-1, :]
+#     H2[:, :, 1, :] .= @views H2[:, :, 2, :]
+#     H3[:, :, 1:end-1, :] .= @views M[:, :, 2:end, :]
+#     H3[:, :, end, :] .= @views H3[:, :, end-1, :]
+
+#     @. @views H_exch .= exch / dd / dd * (H0 + H1 + H2 + H3 - 4 * M)
+
+# end
 
 
     
