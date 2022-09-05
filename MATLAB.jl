9f762bcd17895835c2cabd7e2760c0279bd09100
Dreycen Foiles <foilesdreycen@gmail.com>
2022-09-05 01:40:04 -0500

    Optimized FFTs

--- MATLAB.jl
+++ MATLAB.jl
@@ -1,16 +1,15 @@
-# using FFTW
 using Statistics
 using CUDA
 using CUDA.CUFFT
 using DifferentialEquations
 using Plots
-# using ProfileView
+using LinearAlgebra
 
 using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 global_logger(TerminalLogger())
 
-CUDA.allowscalar(true)
+CUDA.allowscalar(false)
 
 nx = 166; # number of cells on x direction
 ny = 42;
@@ -44,49 +43,40 @@ deltaMy = CUDA.zeros(nx, ny, nz);
 deltaMz = CUDA.zeros(nx, ny, nz);
 mag = CUDA.zeros(nx, ny, nz);
 
-Mx_pad = CUDA.zeros(nx * 2, ny * 2, nz * 2);
-My_pad = CUDA.zeros(nx * 2, ny * 2, nz * 2);
-Mz_pad = CUDA.zeros(nx * 2, ny * 2, nz * 2);
+Mx_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
+My_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
+Mz_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
 
-Hx_pad = CUDA.zeros(nx * 2, ny * 2, nz * 2);
-Hy_pad = CUDA.zeros(nx * 2, ny * 2, nz * 2);
-Hz_pad = CUDA.zeros(nx * 2, ny * 2, nz * 2);
+Mx_fft = CUDA.zeros(ComplexF64, nx + 1, ny * 2, nz * 2);
+My_fft = CUDA.zeros(ComplexF64, nx + 1, ny * 2, nz * 2)
+Mz_fft = CUDA.zeros(ComplexF64, nx + 1, ny * 2, nz * 2)
 
+Hx_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
+Hy_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
+Hz_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
 
-mx_avg = CUDA.zeros(floor(Int, timesteps / 1000))
-my_avg = CUDA.zeros(floor(Int, timesteps / 1000))
-mz_avg = CUDA.zeros(floor(Int, timesteps / 1000))
+MxHx = CUDA.zeros(Float64, nx, ny, nz);
+MxHy = CUDA.zeros(Float64, nx, ny, nz);
+MxHz = CUDA.zeros(Float64, nx, ny, nz);
 
-Kxx = zeros(nx * 2, ny * 2, nz * 2); # Initialization of demagnetization tensor
-Kxy = zeros(nx * 2, ny * 2, nz * 2);
-Kxz = zeros(nx * 2, ny * 2, nz * 2);
-Kyy = zeros(nx * 2, ny * 2, nz * 2);
-Kyz = zeros(nx * 2, ny * 2, nz * 2);
-Kzz = zeros(nx * 2, ny * 2, nz * 2);
+Kxx = zeros(Float64, nx * 2, ny * 2, nz * 2); # Initialization of demagnetization tensor
+Kxy = zeros(Float64, nx * 2, ny * 2, nz * 2);
+Kxz = zeros(Float64, nx * 2, ny * 2, nz * 2);
+Kyy = zeros(Float64, nx * 2, ny * 2, nz * 2);
+Kyz = zeros(Float64, nx * 2, ny * 2, nz * 2);
+Kzz = zeros(Float64, nx * 2, ny * 2, nz * 2);
 prefactor = 1 / 4 / 3.14159265;
 
 include("Demag.jl")
+include("Exchange.jl")
 
 
+H_exch = CUDA.zeros(3, nx, ny, nz);
 
-Hx_exch = CUDA.zeros(nx, ny, nz);
-Hy_exch = CUDA.zeros(nx, ny, nz);
-Hz_exch = CUDA.zeros(nx, ny, nz);
-
-# outFile = open("Mdata.txt", "w");
-
-Hx0 = CUDA.zeros(nx, ny, nz);
-Hx1 = CUDA.zeros(nx, ny, nz);
-Hx2 = CUDA.zeros(nx, ny, nz);
-Hx3 = CUDA.zeros(nx, ny, nz);
-Hy0 = CUDA.zeros(nx, ny, nz);
-Hy1 = CUDA.zeros(nx, ny, nz);
-Hy2 = CUDA.zeros(nx, ny, nz);
-Hy3 = CUDA.zeros(nx, ny, nz);
-Hz0 = CUDA.zeros(nx, ny, nz);
-Hz1 = CUDA.zeros(nx, ny, nz);
-Hz2 = CUDA.zeros(nx, ny, nz);
-Hz3 = CUDA.zeros(nx, ny, nz);
+H0 = CUDA.zeros(3, nx, ny, nz);
+H1 = CUDA.zeros(3, nx, ny, nz);
+H2 = CUDA.zeros(3, nx, ny, nz);
+H3 = CUDA.zeros(3, nx, ny, nz);
 
 function LLG_loop!(dm, m0, p, t)
 
@@ -95,78 +85,55 @@ function LLG_loop!(dm, m0, p, t)
     global prefactor2
     global mag
 
-    Mx = m0[1, :, :, :]
-    My = m0[2, :, :, :]
-    Mz = m0[3, :, :, :]
+    Mx = @views m0[1, :, :, :]
+    My = @views m0[2, :, :, :]
+    Mz = @views m0[3, :, :, :]
 
     fill!(Mx_pad, 0)
     fill!(My_pad, 0)
     fill!(Mz_pad, 0)
 
-    Mx_pad[1:nx, 1:ny, 1:nz] = Mx
-    My_pad[1:nx, 1:ny, 1:nz] = My
-    Mz_pad[1:nx, 1:ny, 1:nz] = Mz
+    fill!(H_exch, 0)
 
-    # Hx_pad = irfft(rfft(Mx_pad) .* Kxx_fft + rfft(My_pad) .* Kxy_fft + rfft(Mz_pad) .* Kxz_fft, 2 * nx) # calc demag field with fft
-    Hx_pad = iplan * (
-        (plan * Mx_pad) .* Kxx_fft +
-        (plan * My_pad) .* Kxy_fft +
-        (plan * Mz_pad) .* Kxz_fft
+    Mx_pad[1:nx, 1:ny, 1:nz] .= Mx
+    My_pad[1:nx, 1:ny, 1:nz] .= My
+    Mz_pad[1:nx, 1:ny, 1:nz] .= Mz
+
+    mul!(Mx_fft, plan, Mx_pad)
+    mul!(My_fft, plan, My_pad)
+    mul!(Mz_fft, plan, Mz_pad)
+
+    mul!(Hx_pad, iplan,
+        Mx_fft .* Kxx_fft .+
+        My_fft .* Kxy_fft .+
+        Mz_fft .* Kxz_fft
     ) # calc demag field with fft
 
-    Hy_pad = iplan * (
-        (plan * Mx_pad) .* Kxy_fft +
-        (plan * My_pad) .* Kyy_fft +
-        (plan * Mz_pad) .* Kyz_fft
+    mul!(Hy_pad, iplan,
+        Mx_fft .* Kxy_fft .+
+        My_fft .* Kyy_fft .+
+        Mz_fft .* Kyz_fft
     )
 
-    Hz_pad = iplan * (
-        (plan * Mx_pad) .* Kxz_fft +
-        (plan * My_pad) .* Kyz_fft +
-        (plan * Mz_pad) .* Kzz_fft
+    mul!(Hz_pad, iplan,
+        Mx_fft .* Kxz_fft .+
+        My_fft .* Kyz_fft .+
+        Mz_fft .* Kzz_fft
     )
-    # Hy_pad = irfft(rfft(Mx_pad) .* Kxy_fft + rfft(My_pad) .* Kyy_fft + rfft(Mz_pad) .* Kyz_fft, 2 * nx)
-    # Hz_pad = irfft(rfft(Mx_pad) .* Kxz_fft + rfft(My_pad) .* Kyz_fft + rfft(Mz_pad) .* Kzz_fft, 2 * nx)
+
+
     Hx = real(Hx_pad[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)]) # truncation of demag field
     Hy = real(Hy_pad[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
     Hz = real(Hz_pad[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
-    # Mx = Mx_pad[1:nx, 1:ny, 1:nz] # truncation of Mx, remove zero padding
-    # My = My_pad[1:nx, 1:ny, 1:nz]
-    # Mz = Mz_pad[1:nx, 1:ny, 1:nz]
-    # calculation of exchange field
-    Hx0[2:end, :, :] = Mx[1:end-1, :, :]
-    Hx0[1, :, :] = Hx0[2, :, :]
-    Hx1[1:end-1, :, :] = Mx[2:end, :, :]
-    Hx1[end, :, :] = Hx1[end-1, :, :]
 
-    Hx2[:, 2:end, :] = Mx[:, 1:end-1, :]
-    Hx2[:, 1, :] = Hx2[:, 2, :]
-    Hx3[:, 1:end-1, :] = Mx[:, 2:end, :]
-    Hx3[:, end, :] = Hx3[:, end-1, :]
+    Exchange!(H_exch, m0, exch, H0, H1, H2, H3, dd)
+    Hx_exch = @view H_exch[1, :, :, :]
+    Hy_exch = @view H_exch[2, :, :, :]
+    Hz_exch = @view H_exch[3, :, :, :]
 
-    Hy0[2:end, :, :] = My[1:end-1, :, :]
-    Hy0[1, :, :] = Hy0[2, :, :]
-    Hy1[1:end-1, :, :] = My[2:end, :, :]
-    Hy1[end, :, :] = Hy1[end-1, :, :]
-
-    Hy2[:, 2:end, :] = My[:, 1:end-1, :]
-    Hy2[:, 1, :] = Hy2[:, 2, :]
-    Hy3[:, 1:end-1, :] = My[:, 2:end, :]
-    Hy3[:, end, :] = Hy3[:, end-1, :]
-
-    Hz0[2:end, :, :] = Mz[1:end-1, :, :]
-    Hz0[1, :, :] = Hz0[2, :, :]
-    Hz1[1:end-1, :, :] = Mz[2:end, :, :]
-    Hz1[end, :, :] = Hz1[end-1, :, :]
-
-    Hz2[:, 2:end, :] = Mz[:, 1:end-1, :]
-    Hz2[:, 1, :] = Hz2[:, 2, :]
-    Hz3[:, 1:end-1, :] = Mz[:, 2:end, :]
-    Hz3[:, end, :] = Hz3[:, end-1, :]
-
-    Hx += exch / dd / dd * (Hx0 + Hx1 + Hx2 + Hx3 - 4 * Mx)
-    Hy += exch / dd / dd * (Hy0 + Hy1 + Hy2 + Hy3 - 4 * My)
-    Hz += exch / dd / dd * (Hz0 + Hz1 + Hz2 + Hz3 - 4 * Mz)
+    Hx += Hx_exch
+    Hy += Hy_exch
+    Hz += Hz_exch
 
     if t < 4000
         Hx .+= 100 # apply a saturation field to get S-state
@@ -184,36 +151,14 @@ function LLG_loop!(dm, m0, p, t)
         prefactor2 = prefactor1 * alpha / Ms
     end
     # apply LLG equation
-    MxHx = My .* Hz - Mz .* Hy # = M cross H
-    MxHy = Mz .* Hx - Mx .* Hz
-    MxHz = Mx .* Hy - My .* Hx
+    @. MxHx = My * Hz - Mz * Hy # = M cross H
+    @. MxHy = Mz * Hx - Mx * Hz
+    @. MxHz = Mx * Hy - My * Hx
 
-    deltaMx = prefactor1 .* MxHx + prefactor2 .* (My .* MxHz - Mz .* MxHy)
-    deltaMy = prefactor1 .* MxHy + prefactor2 .* (Mz .* MxHx - Mx .* MxHz)
-    deltaMz = prefactor1 .* MxHz + prefactor2 .* (Mx .* MxHy - My .* MxHx)
+    @. dm[1, :, :, :] = prefactor1 * MxHx + prefactor2 * (My * MxHz - Mz * MxHy)
+    @. dm[2, :, :, :] = prefactor1 * MxHy + prefactor2 * (Mz * MxHx - Mx * MxHz)
+    @. dm[3, :, :, :] = prefactor1 * MxHz + prefactor2 * (Mx * MxHy - My * MxHx)
 
-    dm[1, :, :, :] = deltaMx
-    dm[2, :, :, :] = deltaMy
-    dm[3, :, :, :] = deltaMz
-
-    # Mx += deltaMx
-    # My += deltaMy
-    # Mz += deltaMz
-
-    # mag = sqrt.(Mx .* Mx + My .* My + Mz .* Mz)
-    # Mx = Mx ./ mag * Ms
-    # My = My ./ mag * Ms
-    # Mz = Mz ./ mag * Ms
-
-    # if t % 1000 == 0 # recod the average magnetization
-    #     print(t)
-    #     MxMean = mean(Mx)
-    #     MyMean = mean(My)
-    #     MzMean = mean(Mz)
-    #     mx_avg[floor(Int,t/1000)] = MxMean
-    #     my_avg[floor(Int,t/1000)] = MyMean
-    #     mz_avg[floor(Int,t/1000)] = MzMean
-    # write(outFile, "#d\t#f\t#f\t#f\r\n", t, MxMean / Ms, MyMean / Ms, MzMean / Ms)
 end
 # close(outFile);
 
@@ -222,22 +167,23 @@ tspan = (0, end_point)
 t_range = range(0, end_point, length=300)
 
 m0 = CUDA.zeros(3, nx, ny, nz)
-m0[1, :, :, :] = Mx
-m0[2, :, :, :] = My
-m0[3, :, :, :] = Mz
+m0[1, :, :, :] .= Mx
+m0[2, :, :, :] .= My
+m0[3, :, :, :] .= Mz
 
 p = ()
 
 
 prob = ODEProblem(LLG_loop!, m0, tspan, p)
-# @profview sol = solve(prob, BS3(), progress=true, progress_steps=100)
-sol = solve(prob, BS3(), progress=true, progress_steps=100)
+# @profview sol = solve(prob, OwrenZen3(), progress=true, progress_steps=100)
+# @profview sol = solve(prob, OwrenZen3(), progress=true, progress_steps=100)
+sol = solve(prob, BS3(), progress=true, progress_steps=200)
 
 
 
 # The '...' is absolutely necessary here. It's called splatting and I don't know 
 # how it works.
-cpu_sol = cat([Array(x) for x in sol.u]...,dims=5)
+cpu_sol = cat([Array(x) for x in sol.u]..., dims=5)
 
 mx_vals = cpu_sol[1, 1:nx, 1:ny, 1:nz, :]
 my_vals = cpu_sol[2, 1:nx, 1:ny, 1:nz, :]
