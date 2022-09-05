6f9f697e0acace4dee25df2d169ed7c9d17ff28d
Dreycen Foiles <foilesdreycen@gmail.com>
2022-09-05 12:16:49 -0500

    GPU Kernel method for Exchange calculation

--- MATLAB.jl
+++ MATLAB.jl
@@ -16,32 +16,14 @@ ny = 42;
 nz = 1;
 dd = 3; # cell volume = dd x dd x dd
 dt = 5E-6; # timestep in nanoseconds
-# dt = CuArray(dt); # copy data from CPU to GPU
 timesteps = 150000;
 alpha = 0.5; # damping constant to relax system to S-state
-# alpha = CuArray(alpha);
 exchConstant = 1.3E-11 * 1E18; # nanometer/nanosecond units
 mu_0 = 1.256636; # vacuum permeability, = 4 * pi / 10
 Ms = 800; # saturation magnetization
-# Ms = CuArray(Ms);
 exch = 2 * exchConstant / mu_0 / Ms / Ms;
-# exch = CuArray(exch);
 prefactor1 = (-0.221) * dt / (1 + alpha * alpha);
 prefactor2 = prefactor1 * alpha / Ms;
-# prefactor1 = CuArray(prefactor1);
-# prefactor2 = CuArray(prefactor2);
-Mx = ones(nx, ny, nz) .* Ms; # magnetization on x direction 
-My = zeros(nx, ny, nz); # magnetization on y direction
-Mz = zeros(nx, ny, nz); # magnetization on z direction
-
-Mx = CuArray(Mx);
-My = CuArray(My);
-Mz = CuArray(Mz);
-
-deltaMx = CUDA.zeros(nx, ny, nz);
-deltaMy = CUDA.zeros(nx, ny, nz);
-deltaMz = CUDA.zeros(nx, ny, nz);
-mag = CUDA.zeros(nx, ny, nz);
 
 Mx_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
 My_pad = CUDA.zeros(Float64, nx * 2, ny * 2, nz * 2);
@@ -65,7 +47,7 @@ Kxz = zeros(Float64, nx * 2, ny * 2, nz * 2);
 Kyy = zeros(Float64, nx * 2, ny * 2, nz * 2);
 Kyz = zeros(Float64, nx * 2, ny * 2, nz * 2);
 Kzz = zeros(Float64, nx * 2, ny * 2, nz * 2);
-prefactor = 1 / 4 / 3.14159265;
+prefactor = 1 / 4 / pi;
 
 include("Demag.jl")
 include("Exchange.jl")
@@ -83,7 +65,6 @@ function LLG_loop!(dm, m0, p, t)
     global alpha
     global prefactor1
     global prefactor2
-    global mag
 
     Mx = @views m0[1, :, :, :]
     My = @views m0[2, :, :, :]
@@ -126,7 +107,8 @@ function LLG_loop!(dm, m0, p, t)
     Hy = real(Hy_pad[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
     Hz = real(Hz_pad[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
 
-    Exchange!(H_exch, m0, exch, H0, H1, H2, H3, dd)
+    # Exchange!(H_exch, m0, exch, H0, H1, H2, H3, dd)
+    Exchange!(H_exch, m0, exch, dd, dd, dd)
     Hx_exch = @view H_exch[1, :, :, :]
     Hy_exch = @view H_exch[2, :, :, :]
     Hz_exch = @view H_exch[3, :, :, :]
@@ -160,25 +142,21 @@ function LLG_loop!(dm, m0, p, t)
     @. dm[3, :, :, :] = prefactor1 * MxHz + prefactor2 * (Mx * MxHy - My * MxHx)
 
 end
-# close(outFile);
 
 end_point = 200000
 tspan = (0, end_point)
 t_range = range(0, end_point, length=300)
 
 m0 = CUDA.zeros(3, nx, ny, nz)
-m0[1, :, :, :] .= Mx
-m0[2, :, :, :] .= My
-m0[3, :, :, :] .= Mz
+m0[1, :, :, :] .= Ms
 
 p = ()
 
 
 prob = ODEProblem(LLG_loop!, m0, tspan, p)
 # @profview sol = solve(prob, OwrenZen3(), progress=true, progress_steps=100)
-# @profview sol = solve(prob, OwrenZen3(), progress=true, progress_steps=100)
-sol = solve(prob, BS3(), progress=true, progress_steps=200)
-
+@profview sol = solve(prob, BS3(), progress=true, progress_steps=300)
+# sol = solve(prob, BS3(), progress=true, progress_steps=200);
 
 
 # The '...' is absolutely necessary here. It's called splatting and I don't know 
