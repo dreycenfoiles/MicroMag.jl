e6140cd3f4fba35ba12e5e1f850d888617a3dda5
Dreycen Foiles <foilesdreycen@gmail.com>
2022-10-12 00:18:48 -0500

    Fixed normalize check

--- LLG.jl
+++ LLG.jl
@@ -19,7 +19,8 @@ dx = 3; # cell volume = dd x dd x dd
 dy = 3; # cell volume = dd x dd x dd
 dz = 3; # cell volume = dd x dd x dd
 
-mu_0 = pi*4e-7/1e9; # vacuum permeability, = 4 * pi / 10
+const mu_0 = pi * 4e-7 / 1e9; # vacuum permeability, = 4 * pi / 10
+const gamma = 2.221e5
 
 Mx_pad = CUDA.zeros(Float32, nx * 2, ny * 2, nz * 2);
 My_pad = CUDA.zeros(Float32, nx * 2, ny * 2, nz * 2);
@@ -28,6 +29,10 @@ Mz_pad = CUDA.zeros(Float32, nx * 2, ny * 2, nz * 2);
 Mx_fft = CUDA.zeros(ComplexF32, nx + 1, ny * 2, nz * 2);
 My_fft = CUDA.zeros(ComplexF32, nx + 1, ny * 2, nz * 2)
 Mz_fft = CUDA.zeros(ComplexF32, nx + 1, ny * 2, nz * 2)
+
+Hx_demag_fft = CUDA.zeros(ComplexF32, nx + 1, ny * 2, nz * 2)
+Hy_demag_fft = CUDA.zeros(ComplexF32, nx + 1, ny * 2, nz * 2)
+Hz_demag_fft = CUDA.zeros(ComplexF32, nx + 1, ny * 2, nz * 2)
 
 plan = plan_rfft(Mx_pad);
 iplan = plan_irfft(Mx_fft, 2 * nx);
@@ -55,7 +60,7 @@ function LLG_loop!(dm, m0, p, t)
 
     Ms, A, alpha = p
 
-    prefactor1 = -2.221e5 / (1 + alpha * alpha)
+    prefactor1 = -gamma / (1 + alpha * alpha)
     prefactor2 = prefactor1 * alpha
     exch = 2 * A / mu_0 / Ms
 
@@ -75,35 +80,25 @@ function LLG_loop!(dm, m0, p, t)
     fill!(My_pad, 0)
     fill!(Mz_pad, 0)
 
-    Mx_pad[1:nx, 1:ny, 1:nz] = Mx .* Ms
-    My_pad[1:nx, 1:ny, 1:nz] = My .* Ms
-    Mz_pad[1:nx, 1:ny, 1:nz] = Mz .* Ms
+    @inbounds Mx_pad[1:nx, 1:ny, 1:nz] = Mx .* Ms
+    @inbounds My_pad[1:nx, 1:ny, 1:nz] = My .* Ms
+    @inbounds Mz_pad[1:nx, 1:ny, 1:nz] = Mz .* Ms
+
+    @. Hx_demag_fft = Mx_fft * Kxx_fft + My_fft .* Kxy_fft + Mz_fft * Kxz_fft # calc demag field with fft
+    @. Hy_demag_fft = Mx_fft * Kxy_fft + My_fft .* Kyy_fft + Mz_fft * Kyz_fft
+    @. Hz_demag_fft = Mx_fft * Kxz_fft + My_fft .* Kyz_fft + Mz_fft * Kzz_fft
 
     mul!(Mx_fft, plan, Mx_pad)
     mul!(My_fft, plan, My_pad)
     mul!(Mz_fft, plan, Mz_pad)
 
-    mul!(Hx_demag, iplan,
-        Mx_fft .* Kxx_fft .+
-        My_fft .* Kxy_fft .+
-        Mz_fft .* Kxz_fft
-    ) # calc demag field with fft
+    mul!(Hx_demag, iplan, Hx_demag_fft)
+    mul!(Hy_demag, iplan, Hy_demag_fft)
+    mul!(Hz_demag, iplan, Hz_demag_fft)
 
-    mul!(Hy_demag, iplan,
-        Mx_fft .* Kxy_fft .+
-        My_fft .* Kyy_fft .+
-        Mz_fft .* Kyz_fft
-    )
-
-    mul!(Hz_demag, iplan,
-        Mx_fft .* Kxz_fft .+
-        My_fft .* Kyz_fft .+
-        Mz_fft .* Kzz_fft
-    )
-
-    Hx_eff .= real(Hx_demag[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)]) # truncation of demag field
-    Hy_eff .= real(Hy_demag[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
-    Hz_eff .= real(Hz_demag[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
+    @inbounds Hx_eff .= real(Hx_demag[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)]) # truncation of demag field
+    @inbounds Hy_eff .= real(Hy_demag[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
+    @inbounds Hz_eff .= real(Hz_demag[nx:(2*nx-1), ny:(2*ny-1), nz:(2*nz-1)])
 
     ####################
     ## Exchange Field ##
@@ -113,17 +108,18 @@ function LLG_loop!(dm, m0, p, t)
 
     H_eff .+= H_exch
 
-    if t < .05
-        Hx_eff .+= .1/1e18/mu_0 # apply a saturation field to get S-state
-        Hy_eff .+= .1/1e18/mu_0
-        Hz_eff .+= .1/1e18/mu_0
-    elseif t > .25
-        Hx_eff .+= -24.6e-3/1e18/mu_0 # apply the reverse field
-        Hy_eff .+= +4.3e-3/1e18/mu_0
+    if t < 0.05
+        Hx_eff .+= 0.1 / 1e18 / mu_0 # apply a saturation field to get S-state
+        Hy_eff .+= 0.1 / 1e18 / mu_0
+        Hz_eff .+= 0.1 / 1e18 / mu_0
+    elseif t > 0.25
+        Hx_eff .+= -24.6e-3 / 1e18 / mu_0 # apply the reverse field
+        Hy_eff .+= +4.3e-3 / 1e18 / mu_0
         alpha = 0.02
-        prefactor1 = -2.221e5 / (1 + alpha * alpha)
+        prefactor1 = -gamma / (1 + alpha * alpha)
         prefactor2 = prefactor1 * alpha
     end
+    
     # apply LLG equation
     @. MxHx = My * Hz_eff - Mz * Hy_eff # = M cross H
     @. MxHy = Mz * Hx_eff - Mx * Hz_eff
@@ -137,29 +133,31 @@ function LLG_loop!(dm, m0, p, t)
 
 end
 
-# function check_normalize!(m,Ms)
-#     nc,nx,ny,nz = size(m)
+function check_normalize!(m)
+    nc, nx, ny, nz = size(m)
 
-#     for index in CartesianIndices((nx,ny,nz))
-#         current_m = m[:, index]
-#         if norm
+    for index in CartesianIndices((nx, ny, nz))
+        current_m = @views m[:, index]
+        if norm(current_m) != 1
+            normalize!(current_m)
+        end
 
-#         end
-
-#     end
-# end
+    end
+end
 
 
 end_point = 1
 tspan = (0, end_point)
 
 alpha = 0.5; # damping constant to relax system to S-state
-A = 1.3E-11/1e9; # nanometer/nanosecond units
-Ms = 8e5/1e9; # saturation magnetization
+A = 1.3E-11 / 1e9; # nanometer/nanosecond units
+Ms = 8e5 / 1e9; # saturation magnetization
 p = (Ms, A, alpha)
 
 m0 = CUDA.zeros(Float32, 3, nx, ny, nz)
+m0[1, :, :, :] .= 1
 m0[2, :, :, :] .= 1
+check_normalize!(m0)
 
 
 prob = ODEProblem(LLG_loop!, m0, tspan, p);
