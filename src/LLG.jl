596ea6bf21ed724f3c62b928a060f54ee9fa1c89
Dreycen Foiles <foilesdreycen@gmail.com>
2022-12-14 21:54:46 -0600

    Zeeman is finished

--- src/LLG.jl
+++ src/LLG.jl
@@ -1,15 +1,12 @@
 
-function LLG_loop!(dm, m0, p, t)
+function LLG_loop!(dm, m0, p, t::Float64)
 
     mesh, fields, demag, param = p
 
-    Ms, A, alpha = param
+    Ms, A, α = param
 
-    Hx_eff = @view fields.H_eff[1, :, :, :]
-    Hy_eff = @view fields.H_eff[2, :, :, :]
-
-    prefactor1 = -γ / (1 + alpha * alpha)
-    prefactor2 = prefactor1 * alpha
+    prefactor1 = -γ / (1 + α * α)
+    prefactor2 = prefactor1 * α
     exch = 2 * A / μ₀ / Ms
 
     #################
@@ -27,15 +24,7 @@ function LLG_loop!(dm, m0, p, t)
 
     Exchange!(fields.H_eff, m0, exch, mesh)
 
-    if t < 0.05
-        fields.H_eff .+= 0.1 / 1e18 / μ₀ # apply a saturation field to get S-state
-    elseif t > 0.25
-        Hx_eff .+= -24.6e-3 / 1e18 / μ₀ # apply the reverse field
-        Hy_eff .+= +4.3e-3 / 1e18 / μ₀
-        alpha = 0.02
-        prefactor1 = -γ / (1 + alpha * alpha)
-        prefactor2 = prefactor1 * alpha
-    end
+    Zeeman!(fields.H_eff,  B_ext(t))
 
     # apply LLG equation
     cross!(fields.M_x_H, m0, fields.H_eff)
--- src/Zeeman.jl
+++ src/Zeeman.jl
@@ -1,10 +1,18 @@
 
-
-function Zeeman!(H_eff::CuArray{Float32, 4}, H_ext::Function, t::Float64)
-
-    H_eff .+= H_ext(t)
+function Zeeman!(H_eff::CuArray{Float32,4}, B_ext::CuArray{Float32,1})
     
+    H_eff[1, :, :, :] .+= B_ext[1] / 1e18 / μ₀
+    H_eff[2, :, :, :] .+= B_ext[2] / 1e18 / μ₀
+    H_eff[3, :, :, :] .+= B_ext[3] / 1e18 / μ₀
+    
+    nothing
+
 end
 
-# TODO: Add constant field term 
-# TODO: Add spatially dependent term 
+function Zeeman!(H_eff::CuArray{Float32,4}, B_ext::CuArray{Float32,4})
+    
+    H_eff .+= B_ext ./ 1e18 ./ μ₀
+    
+    nothing
+
+end 
